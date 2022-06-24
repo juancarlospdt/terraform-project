6014919687fbc8950ce504dd320db1df4a8cb6b8
juancarlospdt <don-vitocarlone@hotmail.com>
2022-06-24 05:53:06 +0200

    2.0

--- main.tf
+++ main.tf
@@ -1,7 +1,20 @@
+terraform {
+required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }       
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-features {}
-subscription_id = "5eee38bf-2553-485f-96ad-b473079ef5a3"
-client_id = "ac19a403-3b47-49ea-8972-bdd864f27886" # appID
-client_secret = "QJi8Q~dKeCEvd22NoM23bbTtUtJVW3vK9m9e-b8k" # password
-tenant_id = "1ed01348-96b5-4e18-862c-3199ab08f449" # tenant
+  skip_provider_registration = "true"
+  subscription_id = "c9cac4e4-7711-43df-a3a7-35f3b86a45ee"
+  features {}
+}
+# Create a resource group
+resource "azurerm_resource_group" "terra-linux" {
+  name = "terra-linux-rg"
+  location = "West Europe"
 }
